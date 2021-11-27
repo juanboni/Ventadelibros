84990db940a2b3021a178c4a61f47975095c3c9a
juanboni <52544365+juanboni@users.noreply.github.com>
2021-11-27 12:00:49 -0500

    gestion

--- Ventadelibros/src/ventadelibros/vistaventalibros.java
+++ Ventadelibros/src/ventadelibros/vistaventalibros.java
@@ -10,6 +10,9 @@ import Modelo.Cliente;
 import Modelo.Compra;
 import Modelo.Digital;
 import Modelo.Impreso;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
 import java.util.Scanner;
 
 /**
@@ -42,10 +45,27 @@ public class vistaventalibros {
         
         //Compra cm=new Compra(1, "sdasd", 1, 1);
         //System.out.print(cm.getId()+","+cm.getFecha()+","+cm.getLibroid()+","+cm.getClienteid());
-        Cliente c=new Cliente(0, "juan boni", "0302882725", "azogues", 12.55);
+        Cliente c=new Cliente(0, "janneth", "0302882725", "azogues", 12.55);
         Gestioncliente gc=new Gestioncliente();
         //gc.insertcliente(c);
         gc.leercliente();
+        //se genera el ticket
+        
+        //insertamos compra
+        String fechaHora;
+        DateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+            Calendar cal = Calendar.getInstance();
+            java.util.Date date = cal.getTime();
+         fechaHora = dateFormat.format(date);
+        Compra cmd=new Compra(0, fechaHora, 1, 0, 1);
+        
+        Gestioncompra gcm=new Gestioncompra();
+        //gcm.insertcompradig(cmd);
+        
+        
+        Compra cmi=new Compra(0, fechaHora, 0,1 , 2);
+        gcm.insertcompraimp(cmi);
+        gcm.leercompra();
     }
     
 }
